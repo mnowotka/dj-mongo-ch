974b9178b4a752dbb6f4c583d5e5c91fad406832
Michał Nowotka <mmmnow@gmail.com>
2013-11-30 12:31:17 +0000

    compression and chunking implementation
    This change introduces:
     - compression of cache contents before storing them into MongoDB
     - chunking large content to overcome 16MB BSON limit
    
    Some minor changes include:
     - authentication
     - connection and socket timeout

--- mongo_cache/backend.py
+++ mongo_cache/backend.py
@@ -7,10 +7,16 @@ except ImportError:
     import pickle
 import base64
 import pymongo
-import re
-from pymongo.errors import OperationFailure, TimeoutError
 from datetime import datetime, timedelta
 from django.core.cache.backends.base import BaseCache
+import zlib
+import logging
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+MAX_SIZE = 16000000
+
+#-----------------------------------------------------------------------------------------------------------------------
 
 class MongoDBCache(BaseCache):
 
@@ -23,26 +29,31 @@ class MongoDBCache(BaseCache):
         self._database = options.get('DATABASE', 'django_cache')
         self._rshosts = options.get('RSHOSTS')
         self._rsname = options.get('RSNAME')
+        self._user = options.get('USER', None)
+        self._password = options.get('PASSWORD', None)
+        self._socket_timeout_ms = options.get('SOCKET_TIMEOUT_MS', None)
+        self._connect_timeout_ms = options.get('CONNECT_TIMEOUT_MS', 20000)
+        self.compression_level = options.get('COMPRESSION_LEVEL', 0)
+        self._tag_sets = options.get('TAG_SETS', None)
         self._read_preference = options.get("READ_PREFERENCE")
         self._collection = location
+        self.log = logging.getLogger(__name__)
 
-    def make_key(self, key, version=None):
-        """
-        Additional regexp to remove $ and . cachaters, as they cause special behaviour in mongodb
-        """
-        key = super(MongoDBCache, self).make_key(key, version)
-        return re.sub(r'\$|\.', '', key)
+#-----------------------------------------------------------------------------------------------------------------------
 
     def add(self, key, value, timeout=None, version=None):
         key = self.make_key(key, version)
         self.validate_key(key)
-        return self._base_set('add', key, value, timeout)
+        self._base_set('add', key, value, timeout)
+
+#-----------------------------------------------------------------------------------------------------------------------
 
     def set(self, key, value, timeout=None, version=None):
         key = self.make_key(key, version)
         self.validate_key(key)
-        return self._base_set('set', key, value, timeout)
+        self._base_set('set', key, value, timeout)
 
+#-----------------------------------------------------------------------------------------------------------------------
 
     def _base_set(self, mode, key, value, timeout=None):
         if not timeout:
@@ -50,23 +61,38 @@ class MongoDBCache(BaseCache):
         now = datetime.utcnow()
         expires = now + timedelta(seconds=timeout)
         coll = self._get_collection()
-        pickled = pickle.dumps(value, pickle.HIGHEST_PROTOCOL)
-        encoded = base64.encodestring(pickled).strip()
+        encoded = self._encode(value)
+        document_size = len(encoded)
         count = coll.count()
         if count > self._max_entries:
             self._cull()
         data = coll.find_one({'key': key})
-        try:
-            if data and (mode == 'set' or
-                    (mode == 'add' and data['expires'] > now)):
-                coll.update({'_id': data['_id']}, {'$set': {'data': encoded, 'expires': expires}}, safe=True)
-            else:
-                coll.insert({'key': key, 'data': encoded, 'expires': expires}, safe=True)
-        #TODO: check threadsafety
-        except (OperationFailure, TimeoutError):
-            return False
+        if data and (mode == 'set' or
+                (mode == 'add' and data['expires'] > now)):
+            coll.update({'_id': data['_id']}, {'$set': {'expires': expires}}, safe=True)
         else:
-            return True
+            if document_size <= MAX_SIZE:
+                coll.insert({'key': key, 'data': encoded, 'expires': expires}, safe=True)
+            else:
+                chunks = []
+                for i in xrange(0, document_size, MAX_SIZE):
+                    chunk = encoded[i:i+MAX_SIZE]
+                    aux_key = self.make_key(chunk)
+                    coll.insert({'key': aux_key, 'data': chunk}, safe=True)
+                    chunks.append(aux_key)
+                coll.insert({'key': key, 'chunks': chunks, 'expires': expires}, safe=True)
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+    def _decode(self, data):
+        return pickle.loads(zlib.decompress(base64.decodestring(data)))
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+    def _encode(self, data):
+        return base64.encodestring(zlib.compress(pickle.dumps(data, pickle.HIGHEST_PROTOCOL), self.compression_level))
+
+#-----------------------------------------------------------------------------------------------------------------------
 
     def get(self, key, default=None, version=None):
         coll = self._get_collection()
@@ -79,9 +105,18 @@ class MongoDBCache(BaseCache):
         if data['expires'] < now:
             coll.remove(data['_id'])
             return default
-        unencoded = base64.decodestring(data['data'])
-        unpickled = pickle.loads(unencoded)
-        return unpickled
+        if data.get('data', None):
+            raw = data['data']
+        elif data.get('chunks', None):
+            raw = ''
+            chunks = data.get('chunks')
+            for chunk in chunks:
+                raw += coll.find_one({'key': chunk})['data']
+        else:
+            return default
+        return self._decode(raw)
+
+#-----------------------------------------------------------------------------------------------------------------------
 
     def get_many(self, keys, version=None):
         coll = self._get_collection()
@@ -97,18 +132,27 @@ class MongoDBCache(BaseCache):
         for result in data:
             if result['expires'] < now:
                 to_remove.append(result['_id'])
-            unencoded = base64.decodestring(result['data'])
-            unpickled = pickle.loads(unencoded)
-            out[parsed_keys[result['key']]] = unpickled
+            elif result.get('data', None):
+                raw = result['data']
+            elif result.get('chunks', None):
+                raw = ''
+                chunks = result.get('chunks')
+                for chunk in chunks:
+                    raw += coll.find_one({'key': chunk})['data']
+            out[parsed_keys[result['key']]] = self._decode(raw)
         if to_remove:
             coll.remove({'_id': {'$in': to_remove}})
         return out
+
+#-----------------------------------------------------------------------------------------------------------------------
 
     def delete(self, key, version=None):
         key = self.make_key(key, version)
         self.validate_key(key)
         coll = self._get_collection()
         coll.remove({'key': key})
+
+#-----------------------------------------------------------------------------------------------------------------------
 
     def has_key(self, key, version=None):
         coll = self._get_collection()
@@ -117,10 +161,13 @@ class MongoDBCache(BaseCache):
         data = coll.find_one({'key': key, 'expires': {'$gt': datetime.utcnow()}})
         return data is not None
 
+#-----------------------------------------------------------------------------------------------------------------------
+
     def clear(self):
         coll = self._get_collection()
         coll.remove(None)
 
+#-----------------------------------------------------------------------------------------------------------------------
 
     def _cull(self):
         if self._cull_frequency == 0:
@@ -130,18 +177,32 @@ class MongoDBCache(BaseCache):
         coll.remove({'expires': {'$lte': datetime.utcnow()}})
         #TODO: implement more agressive cull
 
+#-----------------------------------------------------------------------------------------------------------------------
 
     def _get_collection(self):
         if not getattr(self, '_coll', None):
             self._initialize_collection()
         return self._coll
 
+#-----------------------------------------------------------------------------------------------------------------------
 
     def _initialize_collection(self):
-        #monkey.patch_socket()
+        try:
+            from gevent import monkey
+            monkey.patch_socket()
+        except ImportError:
+            pass
+
         if self._rsname:
-            self.connection = pymongo.MongoReplicaSetClient(self._rshosts, replicaSet=self._rsname, read_preference=self._read_preference)
+            self.connection = pymongo.MongoReplicaSetClient(self._rshosts, replicaSet=self._rsname,
+                read_preference=self._read_preference, socketTimeoutMS=self._socket_timeout_ms,
+                connectTimeoutMS=self._connect_timeout_ms, tag_sets=self._tag_sets)
         else:
             self.connection = pymongo.Connection(self._host, self._port)
+
         self._db = self.connection[self._database]
+        if self._user and self._password:
+            self._db.authenticate(self._user, self._password)
         self._coll= self._db[self._collection]
+
+#-----------------------------------------------------------------------------------------------------------------------
